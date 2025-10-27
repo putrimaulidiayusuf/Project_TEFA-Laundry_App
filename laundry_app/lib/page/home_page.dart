4cf75d948c6203fccc4f89b9f949d4931df221bd
putrimaulidiayusuf <putrimaulidiayusuff@gmail.com>
2025-10-27 13:23:29 +0700

    Add HomePage

---
+++ laundry_app/lib/page/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Text(
+          'Home Page',
+          style: TextStyle(
+            fontSize: 24,
+            color: Colors.black
+          ),
+        ),
+      ),
+    );
+  }
+}
