805018d4458714c46983df21b52a8a82e6b24e0f
putrimaulidiayusuf <putrimaulidiayusuff@gmail.com>
2026-01-26 13:32:35 +0700

    Perbaikan

---
+++ app_laundry/lib/view/page/riwayat_transaksi/tab_selesai.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'widget_no_data.dart';
+
+class TabSelesai extends StatelessWidget {
+  const TabSelesai({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const WidgetNoData();
+  }
+}
--- laundry_app/android/app/src/main/kotlin/com/example/laundry_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.laundry_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
